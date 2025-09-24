f8749b0c46e094f3d1b42b0db3bdf3c91c6e264c
emmanuelknafo <48259636+emmanuelknafo@users.noreply.github.com>
2025-09-24 09:21:14 -0400

    Add Jenkins setup instructions and script for Docker deployment

---
+++ run-jenkins.ps1
@@ -0,0 +1,13 @@
+# Pull the official Jenkins LTS Docker image
+docker pull jenkins/jenkins:lts
+
+# Create a Docker volume for Jenkins data persistence
+docker volume create jenkins_home
+
+# Run Jenkins in Docker, mapping ports and mounting the volume
+docker run -d `
+  --name jenkins `
+  -p 8080:8080 `
+  -p 50000:50000 `
+  -v jenkins_home:/var/jenkins_home `
+  jenkins/jenkins:lts
